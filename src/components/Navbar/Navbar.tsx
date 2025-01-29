5c64cfc0de6e5d4af76c3330893e27a8eb597a4c
varruunnn <varun.choudhary2512@gmail.com>
2025-01-29 08:53:14 +0530

    ok

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,11 +2,19 @@ import Sidebar from "../Sidebar/Sidebar";
 import { useState } from "react";
 import { inAppWallet, createWallet } from "thirdweb/wallets";
 import { darkTheme } from "thirdweb/react";
+import { lightTheme } from "thirdweb/react";
 import { ConnectButton } from "thirdweb/react";
 import { IoMenu, IoClose } from "react-icons/io5";
 import styled from "styled-components";
 import { client } from "../../client";
 const Navbar = () => {
+  const customTheme = lightTheme({
+    colors: {
+      modalBg: "#1E1E1E",
+      accentButtonBg: "#4CAF50",
+      accentButtonText: "white",
+    },
+  });
   const wallets = [
     inAppWallet({
       auth: {
@@ -46,6 +54,7 @@ const Navbar = () => {
   const handleClosePopup = () => {
     setPopupVisible(false);
   };
+  const [isHovered, setIsHovered] = useState(false);
 
   return (
     <div className="max-[485px]:bg-black overflow-x-hidden text-white">
@@ -57,30 +66,35 @@ const Navbar = () => {
         >
           <IoMenu size={37} className="text-white" />
         </button>
-        {/* <button
-          className="w-[120px] h-[41px] bg-[#282828] text-[#82E300] rounded-full hover:bg-[#3a3a3a] transition font-roboto font-semibold"
-          style={{
-            border: "2px solid #82E300",
-            textShadow:
-              "1px 1px 2px black, -1px -1px 2px black, 1px -1px 2px black, -1px 1px 2px black",
-          }}
-          onClick={handleSignup}
+        <div
+          onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
         >
-          Let's Dive In
-        </button> */}
-        <ConnectButton
-          client={client}
-          wallets={wallets}
-          connectModal={{ size: "compact" }}
-          theme={darkTheme({
-            colors: { primaryButtonBg: "hsla(143 100% 43% / 0.85)" },
-          })}
-          connectButton={{ label: "Lets Dive in" }}
-          appMetadata={{
-            name: "Example app",
-            url: "https://example.com",
-          }}
-        />
+          <ConnectButton
+            client={client}
+            wallets={wallets}
+            connectModal={{ size: "compact" }}
+            connectButton={{
+              label: "Lets dive in",
+              style: {
+                backgroundColor: "black",
+                borderRadius: "20000px",
+                color: "white",
+                border: "2px solid #82E300",
+                width: "120px",
+                height: "41px",
+                transition: "box-shadow 0.3s ease-in-out",
+                boxShadow: isHovered
+                  ? "0 0 15px #000000, 0 0 30px #82E300, 0 0 45px #82E300"
+                  : "0 0 10px #82E300, 0 0 20px #82E300",
+              },
+            }}
+            appMetadata={{
+              name: "Example app",
+              url: "https://example.com",
+            }}
+          />
+        </div>
       </header>
       <div
         className={`fixed top-0 left-0 h-full z-50 transition-transform duration-300 ease-in-out ${
@@ -89,71 +103,9 @@ const Navbar = () => {
       >
         <Sidebar onClose={closeSidebar} />
       </div>
-      {isPopupVisible && (
-        <Popup>
-          <div className="popup-content relative h-[300px]">
-            <h1 className="text-[#FED33C] text-xl absolute left-[35%] top-1 font-bold mb-4">
-              Let's Dive In
-            </h1>
-            <button className="close-button" onClick={handleClosePopup}>
-              <IoClose size={24} color="#82E300" />
-            </button>
-            <div className="content relative">
-              <input
-                type="text"
-                placeholder="Enter your email"
-                className="w-[90%] h-[40px] absolute top-[1px] bg-[#C0FFC3] text-[#7A7A7A] p-1 rounded-md mb-4"
-              />
-            </div>
-          </div>
-        </Popup>
-      )}
     </div>
   );
 };
 
-const Popup = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background: rgba(0, 0, 0, 0.7);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  z-index: 1000;
-
-  .popup-content {
-    background: #282828;
-    color: #82e300;
-    padding: 2rem;
-    border-radius: 1rem;
-    width: 400px;
-    box-shadow: 0px 4px 15px rgba(0, 0, 0, 0.2);
-    position: relative;
-  }
-
-  .close-button {
-    position: absolute;
-    top: 1rem;
-    right: 1rem;
-    border: none;
-    background: transparent;
-    cursor: pointer;
-    border: 2px solid #82e300;
-    border-radius: 50%;
-    width: 40px;
-    height: 40px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    transition: transform 0.2s ease;
-
-    &:hover {
-      transform: scale(1.1);
-    }
-  }
-`;
 
 export default Navbar;
