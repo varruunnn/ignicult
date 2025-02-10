ecffe41d85a0571cc9fc62dcfe7affecad3a53cd
varruunnn <varun.choudhary2512@gmail.com>
2025-02-10 22:29:42 +0530

    ok

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -55,18 +55,19 @@ const Navbar: React.FC<NavbarProps> = ({ isSidebarOpen, setSidebarOpen }) => {
   const [isPopupVisible, setPopupVisible] = useState(false);
   const location = useLocation();
   const isPremiumRoute = location.pathname === "/premium-tournaments";
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const routes = [
-    { name: "Home", path: "/home" },
-    { name: "Profile", path: "/profile" },
-    { name: "Games", path: "/games" },
-    { name: "Leaderboard", path: "/leaderboard" },
-    { name: "Tournaments", path: "/tournament" },
-    { name: "Premium Tournaments", path: "/premium-tournaments" },
-    { name: "Activity", path: "/activity" },
-    { name: "Rewards", path: "/rewards" },
-    { name: "Support", path: "/support" },
+    { name: "Home", path: "/home", icon: "/home.svg" },
+    { name: "Profile", path: "/profile", icon: "/profile.svg" },
+    { name: "Games", path: "/games", icon: "/games.svg" },
+    { name: "Leaderboard", path: "/leaderboard", icon: "/leaderboard.svg" },
+    { name: "Tournaments", path: "/tournaments", icon: "/tournament.svg" },
+    { name: "Premium Tournaments", path: "/premium-tournaments", icon: "/premium-tournaments.svg" },
+    { name: "Activity", path: "/activity", icon: "/activity.svg" },
+    { name: "Rewards", path: "/rewards", icon: "/rewards.svg" },
+    { name: "Support", path: "/support", icon: "/support.svg" },
   ];
+
   const togglePopup = () => {
     setPopupVisible((prev) => !prev);
   };
@@ -75,6 +76,7 @@ const Navbar: React.FC<NavbarProps> = ({ isSidebarOpen, setSidebarOpen }) => {
   const closePopup = () => {
     setPopupVisible(false);
   };
+
   const buttonStyle = isPremiumRoute
     ? {
       backgroundColor: "#282828",
@@ -103,80 +105,16 @@ const Navbar: React.FC<NavbarProps> = ({ isSidebarOpen, setSidebarOpen }) => {
     setSidebarOpen(false);
   };
 
-  const handleSignup = () => {
-    setPopupVisible(true);
-  };
-
-  const handleClosePopup = () => {
-    setPopupVisible(false);
-  };
-
   return (
     <div>
-      <div
-        className="max-[485px]:bg-transparent min-[1023px]:hidden overflow-x-hidden text-white
-    min-[1024px]:bg-none
-    "
-      >
+      {/* Mobile sidebar section */}
+      <div className="max-[485px]:bg-transparent min-[1023px]:hidden overflow-x-hidden text-white min-[1024px]:bg-none">
         <motion.header
           initial={{ y: -50, opacity: 0 }}
           animate={{ y: 0, opacity: 1 }}
           transition={{ duration: 0.5 }}
-          className="fixed top-0 left-0 right-0 backdrop-blur-sm flex justify-between items-center px-6 py-4 h-[131px] max-[398px]:z-10 z-50 shadow-md
-
-        min-[1024px]:h-[100px]
-        "
+          className="fixed top-0 left-0 right-0 backdrop-blur-sm flex justify-between items-center px-6 py-4 h-[131px] max-[398px]:z-10 z-50 shadow-md min-[1024px]:h-[100px]"
         >
-          <div className="absolute min-[2559px]:h-[5vh] min-[2559px]:w-[89%] min-[1339px]:h-[10vh] w-[95%] h-[15vh] min-[1440px]:w-[90%] min-[1440px]:left-[10%]">
-            <button className="absolute max-[1023px]:hidden cursor-pointer w-[200px] h-[200px] top-[26%] z-50 left-[28%] "
-              onClick={() => {
-                navigate('/home')
-              }}
-            >
-              <img
-                src="/homee.svg"
-                className=" relative left-[-2%] top-[-44%] w-[20px] z-50 "
-                alt=""
-              />
-              <h3 className="absolute  text-[#9A999C] top-[1%] left-[10%] z-50">
-                Home
-              </h3>
-            </button>
-            <button className="absolute max-[1023px]:hidden cursor-pointer w-[200px] h-[200px] top-[26%] z-50 left-[59%] 
-        max 
-        "
-              onClick={() => {
-                navigate('/games')
-              }}
-            >
-              <img
-                src="/play.svg"
-                className=" relative left-[-2%] top-[-44%] w-[160px] z-50 "
-                alt=""
-              />
-            </button>
-            <div className="absolute max-[1023px]:hidden min-[1024px]:top-[0px] z-100 ">
-              <img
-                src="/vector2.svg"
-                className="max-[1023px]:hidden fixed min-[2559px]:left-[31%] min-[2559px]:w-[40%]  left-[20%] top-[0] z-10 min-[1399px]:left-[27%] rounded-lg"
-                alt="desktopnav"
-              />
-            </div>
-            <button className="absolute max-[1023px]:hidden cursor-pointer w-[200px] h-[200px] top-[26%] z-50 left-[48%] "
-              onClick={() => {
-                navigate('/profile')
-              }}
-            >
-              <img
-                src="/groupp.svg"
-                className=" relative right-[-2%] top-[-44%] w-[20px] z-50 "
-                alt=""
-              />
-              <h3 className="absolute  text-[#9A999C] top-[1%] left-[14%] z-50">
-                Profile
-              </h3>
-            </button>
-          </div>
           <motion.button
             whileHover={{ scale: 1.1 }}
             onClick={toggleSidebar}
@@ -215,68 +153,60 @@ const Navbar: React.FC<NavbarProps> = ({ isSidebarOpen, setSidebarOpen }) => {
         </div>
       </div>
 
-
-
-      <div
-        className="max-[485px]:bg-transparent max-[1023px]:hidden overflow-x-hidden text-white
-    min-[1024px]:bg-none
-    "
-      >
+      <div className="max-[485px]:bg-transparent max-[1023px]:hidden overflow-x-hidden text-white min-[1024px]:bg-none">
         <motion.header
           initial={{ y: -50, opacity: 0 }}
           animate={{ y: 0, opacity: 1 }}
           transition={{ duration: 0.5 }}
-          className="fixed top-0 left-0 right-0 backdrop-blur-sm flex justify-between items-center px-6 py-4 h-[131px] max-[398px]:z-10 z-50 shadow-md
-
-        min-[1024px]:h-[100px]
-        "
+          className="fixed top-0 left-0 right-0 backdrop-blur-sm flex justify-between items-center px-6 py-4 h-[131px] max-[398px]:z-10 z-50 shadow-md min-[1024px]:h-[100px]"
         >
           <div className="absolute min-[2559px]:h-[5vh] min-[2559px]:w-[89%] min-[1339px]:h-[10vh] w-[95%] h-[15vh] min-[1440px]:w-[90%] min-[1440px]:left-[10%]">
-            <button className="absolute max-[1023px]:hidden cursor-pointer w-[200px] h-[200px] top-[26%] z-50 left-[28%] "
+            <button
+              className="absolute max-[1023px]:hidden cursor-pointer w-[60px] h-[40px] top-[26%] z-50 left-[28%]"
               onClick={() => {
-                navigate('/home')
+                navigate("/home");
               }}
             >
               <img
                 src="/homee.svg"
-                className=" relative left-[-2%] top-[-44%] w-[20px] z-50 "
+                className="relative left-[-2%] top-[-20%] w-[20px] z-50"
                 alt=""
               />
-              <h3 className="absolute  text-[#9A999C] top-[1%] left-[10%] z-50">
+              <h3 className="absolute text-[#9A999C] top-[1%] left-[35%] z-50">
                 Home
               </h3>
             </button>
-            <button className="absolute max-[1023px]:hidden cursor-pointer w-[200px] h-[200px] top-[26%] z-50 left-[59%] 
-        max 
-        "
+            <button
+              className="absolute max-[1023px]:hidden cursor-pointer w-[200px] h-[4px] top-[9%] z-50 left-[59%]"
               onClick={() => {
-                navigate('/games')
+                navigate("/games");
               }}
             >
               <img
                 src="/play.svg"
-                className=" relative left-[-2%] top-[-44%] w-[160px] z-50 "
+                className="relative left-[-2%] top-[-44%] w-[160px] z-50"
                 alt=""
               />
             </button>
-            <div className="absolute max-[1023px]:hidden min-[1024px]:top-[0px] z-100 ">
+            <div className="absolute max-[1023px]:hidden min-[1024px]:top-[0px] z-100">
               <img
                 src="/vector2.svg"
-                className="max-[1023px]:hidden fixed min-[2559px]:left-[31%] min-[2559px]:w-[40%]  left-[20%] top-[0] z-10 min-[1399px]:left-[27%] rounded-lg"
+                className="max-[1023px]:hidden fixed min-[2559px]:left-[31%] min-[2559px]:w-[40%] left-[20%] top-[0] z-10 min-[1399px]:left-[27%] rounded-lg"
                 alt="desktopnav"
               />
             </div>
-            <button className="absolute max-[1023px]:hidden cursor-pointer w-[200px] h-[200px] top-[26%] z-50 left-[48%] "
+            <button
+              className="absolute max-[1023px]:hidden cursor-pointer w-[60px] h-[40px] top-[26%] z-50 left-[48%]"
               onClick={() => {
-                navigate('/profile')
+                navigate("/profile");
               }}
             >
               <img
                 src="/groupp.svg"
-                className=" relative right-[-2%] top-[-44%] w-[20px] z-50 "
+                className="relative right-[-2%] top-[-20%] w-[20px] z-50"
                 alt=""
               />
-              <h3 className="absolute  text-[#9A999C] top-[1%] left-[14%] z-50">
+              <h3 className="absolute text-[#9A999C] top-[1%] left-[38%] z-50">
                 Profile
               </h3>
             </button>
@@ -285,7 +215,7 @@ const Navbar: React.FC<NavbarProps> = ({ isSidebarOpen, setSidebarOpen }) => {
             whileHover={{ scale: 1.1 }}
             onClick={togglePopup}
             className="cursor-pointer relative"
-            aria-label="Open Sidebar Menu"
+            aria-label="Open Menu Popup"
           >
             <IoMenu size={37} className="text-white" />
           </motion.button>
@@ -293,6 +223,7 @@ const Navbar: React.FC<NavbarProps> = ({ isSidebarOpen, setSidebarOpen }) => {
             initial={{ scale: 0.95, opacity: 0 }}
             animate={{ scale: 1, opacity: 1 }}
             transition={{ duration: 0.4 }}
+            className="pointer-events-auto relative z-[60]"
           >
             <ConnectButton
               client={client}
@@ -311,39 +242,73 @@ const Navbar: React.FC<NavbarProps> = ({ isSidebarOpen, setSidebarOpen }) => {
             />
           </motion.div>
         </motion.header>
-      {isPopupVisible && (
-        <div className="fixed inset-0 z-[1000] flex items-center justify-center bg-black bg-opacity-50">
-          <motion.div
-            initial={{ scale: 0.8, opacity: 0 }}
-            animate={{ scale: 1, opacity: 1 }}
-            transition={{ duration: 0.3 }}
-            className="bg-[#141414] p-6 rounded-lg w-80 relative"
+        {isPopupVisible && (
+          <div
+            onClick={closePopup}
+            className="fixed inset-0 z-[1000] flex items-center justify-center bg-black bg-opacity-50"
           >
-            <button
-              onClick={closePopup}
-              className="absolute top-2 right-2 text-white text-2xl"
-              aria-label="Close Menu Popup"
+            <motion.div
+              onClick={(e) => e.stopPropagation()} // Prevent closing when clicking inside the popup
+              initial={{ scale: 0.8, opacity: 0 }}
+              animate={{ scale: 1, opacity: 1 }}
+              transition={{ duration: 0.3 }}
+              className="bg-[#141414] p-6 rounded-lg w-[60vw] h-[30vh] relative"
             >
-              &times;
-            </button>
-            <ul className="mt-4 space-y-4">
-              {routes.map((route) => (
-                <li key={route.name}>
-                  <button
-                    onClick={() => {
-                      navigate(route.path);
-                      closePopup();
-                    }}
-                    className="text-white text-lg hover:text-[#82E300] transition-colors"
-                  >
-                    {route.name}
-                  </button>
-                </li>
-              ))}
-            </ul>
-          </motion.div>
-        </div>
-      )}
+              <button
+                onClick={closePopup}
+                className="absolute top-2 right-2 text-white text-2xl"
+                aria-label="Close Menu Popup"
+              >
+                &times;
+              </button>
+              <ul className="mt-4 flex flex-wrap justify-center gap-4">
+                {routes.map((route) => {
+                  const isPremium = route.name === "Premium Tournaments";
+                  return (
+                    <li key={route.name}>
+                      <motion.div
+                        initial={{ opacity: 0, y: 10 }}
+                        animate={{ opacity: 1, y: 0 }}
+                        transition={{ delay: 0.2 }}
+                        whileHover={{ scale: 1.05 }}
+                        className={`${isPremium
+                            ? "border border-[#F94EA6]"
+                            : "border border-[#82E300]"
+                          } rounded-lg`}
+                      >
+                        <motion.button
+                          onClick={() => {
+                            navigate(route.path);
+                            closePopup();
+                          }}
+                          whileHover={{ scale: 1.05, transition: { duration: 0.2 } }}
+                          whileTap={{ scale: 0.95 }}
+                          className="flex items-center p-2 hover:bg-[#282828] transition-colors"
+                        >
+                          <motion.img
+                            src={route.icon}
+                            alt={route.name}
+                            className="w-6 h-6 mr-2"
+                            whileHover={{ rotate: 15 }}
+                          />
+                          <motion.span
+                            className={`text-lg ${isPremium
+                                ? "bg-gradient-to-r from-[#EE49FD] to-[#F94EA6] bg-clip-text text-transparent"
+                                : "text-white"
+                              }`}
+                            whileHover={{ scale: 1.05 }}
+                          >
+                            {route.name}
+                          </motion.span>
+                        </motion.button>
+                      </motion.div>
+                    </li>
+                  );
+                })}
+              </ul>
+            </motion.div>
+          </div>
+        )}
       </div>
     </div>
   );
